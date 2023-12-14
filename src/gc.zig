9dd010c03e5c32df6555dc749b3561a1d289a9c4
Jonathan <sandalmoth@pm.me>
2023-12-14 21:25:25 +0100

    gc prototype, but will probably simplify

---
+++ src/gc.zig
@@ -0,0 +1,133 @@
+const std = @import("std");
+
+const MIN_ALIGN = @max(4, @alignOf(usize)); // s.t. we have 2 bits of pointer tagging guaranteed
+const MIN_SIZE = @sizeOf(usize); // s.t. we can always fit the size in the free list
+
+pub fn Ref(comptime T: type) type {
+    return struct { ptr: *T };
+}
+
+pub const Header = struct {
+    // the header stores the size of an object
+    // meaning, if we're at *T, then adding the
+    data: usize,
+
+    const Status = enum {
+        free,
+        live,
+        move,
+        mark,
+    };
+
+    fn getStatus(h: Header) Status {
+        return switch (h.data & 3) {
+            0 => .free,
+            1 => .live,
+            2 => .move,
+            3 => .mark,
+            else => @panic("bad status"),
+        };
+    }
+
+    fn setStatus(h: *Header, s: Status) void {
+        h.data = h.data & ~3 + switch (s) {
+            .free => 0,
+            .live => 1,
+            .move => 2,
+            .mark => 3,
+        };
+    }
+
+    fn ptr(h: Header) usize {
+        return h & ~3;
+    }
+};
+
+comptime {
+    std.debug.assert(@sizeOf(Header) == @sizeOf(usize));
+    std.debug.assert(@alignOf(Header) == @alignOf(usize));
+    std.debug.assert(@alignOf(usize) <= @sizeOf(usize)); // are there systems where this is false?
+    std.debug.assert(@alignOf(usize) == @alignOf(*usize)); // i think this is true by definition?
+}
+
+pub const Config = struct {
+    page_size: usize = std.mem.page_size,
+    backing_allocator: std.mem.Allocator = std.heap.page_allocator,
+};
+
+pub fn GC(comptime config: Config) type {
+    const Page = struct {
+        const capacity = (config.page_size - @sizeOf(usize)) / @sizeOf(usize);
+
+        data: [capacity]usize = undefined,
+        next: ?*@This() = null,
+    };
+
+    std.debug.assert(@sizeOf(Page) == config.page_size);
+
+    return struct {
+        const Self = @This();
+
+        const backing_allocator: std.mem.Allocator = std.heap.page_allocator;
+        const page_size: usize = config.page_size;
+
+        first_page: ?*Page = null,
+
+        // the free list points to a header
+        // which always stores the size of a block
+        // with the pointer to the next free block folling at the start of the data right after
+        free: ?*Header = null,
+
+        pub fn create(gc: *Self, comptime T: type) Ref(T) {
+            if (gc.first_page == null) {
+                gc.newPage();
+            }
+
+            return Ref(T){ .ptr = undefined };
+
+            // var loc = std.mem.alignBackward(
+            //     usize,
+            //     gc.first_page.?.free -| @sizeOf(T),
+            //     @max(@alignOf(usize), @alignOf(T)),
+            // );
+
+            // if (loc < @intFromPtr(&gc.first_page.?.bytes) + @sizeOf(usize)) {
+            //     if (gc.first_page.?.free == @intFromPtr(&gc.first_page.?.bytes) + Page.capacity) {
+            //         @panic("GC.create: " ++ @typeName(T) ++ " cannot fit in a GC.Page");
+            //     }
+
+            //     // no room on page, so make an new one and find a new alignd address
+            //     gc.newPage();
+            //     return @call(.always_tail, create, .{ gc, T });
+            // }
+
+            // std.debug.print("{} {}\n", .{ @alignOf(T), @sizeOf(T) });
+            // gc.first_page.?.free = loc - @sizeOf(usize);
+
+            // std.debug.print("{}\n", .{loc});
+            // return Ref(T){ .ptr = @ptrFromInt(loc) };
+        }
+
+        fn newPage(gc: *Self) void {
+            var page = backing_allocator.create(Page) catch @panic("GC.newPage: out of memory");
+            page.* = Page{};
+            page.data[0] = Page.capacity;
+            page.data[1] = @intFromPtr(gc.free);
+            gc.free = @ptrCast(&page.data[0]);
+            page.next = gc.first_page;
+            gc.first_page = page;
+        }
+    };
+}
+
+test "scratch" {
+    std.debug.print("\n", .{});
+    var gc = GC(.{ .backing_allocator = std.testing.allocator }){};
+    for (0..1) |_| {
+        _ = gc.create(i32);
+        _ = gc.create(@Vector(4, f64));
+        _ = gc.create(i32);
+        _ = gc.create(u8);
+        _ = gc.create(@Vector(37, f64));
+    }
+}
