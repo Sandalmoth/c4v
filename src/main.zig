e4f6066f700a44b002cc5ea49aad41c37177418a
Jonathan <sandalmoth@pm.me>
2024-07-24 20:16:45 +0200

    add test of just lookup speed

--- src/main.zig
+++ src/main.zig
@@ -12,9 +12,11 @@ pub fn main() !void {
     std.debug.print("hamt-based\n", .{});
     try fuzz7();
     try bench7();
+    try benchLookup7();
     std.debug.print("champ-based\n", .{});
     try fuzz8();
     try bench8();
+    try benchLookup8();
 }
 
 fn scratch() !void {
@@ -84,6 +86,69 @@ fn fuzz7() !void {
     }
 }
 
+fn fuzz8() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    var rng = std.rand.DefaultPrng.init(@bitCast(std.time.microTimestamp()));
+    const rand = rng.random();
+    var pool = try BlockPool.init(std.heap.page_allocator);
+    defer pool.deinit();
+
+    const ns = [_]u32{ 32, 128, 512, 2048, 8192, 32768, 131072, 524288 };
+    const m = 100000;
+    const o = 1000;
+
+    for (ns) |n| {
+        var rt = try RT8.init(&pool);
+        defer rt.deinit();
+        var h = rt.newChamp();
+
+        var s = std.AutoHashMap(u32, u32).init(alloc);
+        defer s.deinit();
+
+        for (0..m) |i| {
+            const x = rand.intRangeLessThan(u32, 0, n);
+            const y = rand.intRangeLessThan(u32, 0, n);
+            const a = rt.newReal(@floatFromInt(x));
+            const b = rt.newReal(@floatFromInt(y));
+
+            std.debug.assert(rt.champContains(h, a) == s.contains(x));
+            if (rt.champContains(h, a)) {
+                std.debug.assert(
+                    @as(u32, @intFromFloat(rt.champGet(h, a).?.as(.real).data)) == s.get(x).?,
+                );
+                const h2 = rt.champDissoc(h, a);
+                h = h2.?;
+                _ = s.remove(x);
+            } else {
+                const h2 = rt.champAssoc(h, a, b);
+                h = h2;
+                try s.put(x, y);
+            }
+
+            const u = rand.intRangeLessThan(u32, 0, n);
+            const v = rand.intRangeLessThan(u32, 0, n);
+            const c = rt.newReal(@floatFromInt(u));
+            const d = rt.newReal(@floatFromInt(v));
+            if (rand.boolean()) {
+                const h2 = rt.champDissoc(h, c);
+                h = h2.?;
+                _ = s.remove(u);
+            } else {
+                const h2 = rt.champAssoc(h, c, d);
+                h = h2;
+                try s.put(u, v);
+            }
+
+            if ((i + 1) % o == 0) {
+                rt.gc.traceRoot(&h);
+                rt.gc.shuffle();
+                try rt.gc.collect();
+            }
+        }
+    }
+}
+
 fn bench7() !void {
     var rng = std.rand.DefaultPrng.init(@bitCast(std.time.microTimestamp()));
     var rand = rng.random();
@@ -136,75 +201,6 @@ fn bench7() !void {
         const ttot = timer.read();
 
         std.debug.print("{}\t{}\t{}\n", .{ n, (ttot - tgc) / m, tgc / m });
-    }
-}
-
-fn fuzz8() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const alloc = gpa.allocator();
-    var rng = std.rand.DefaultPrng.init(@bitCast(std.time.microTimestamp()));
-    const rand = rng.random();
-    var pool = try BlockPool.init(std.heap.page_allocator);
-    defer pool.deinit();
-
-    const ns = [_]u32{ 32, 128, 512, 2048, 8192, 32768, 131072, 524288 };
-    const m = 100000;
-    const o = 1000;
-
-    for (ns) |n| {
-        var rt = try RT8.init(&pool);
-        defer rt.deinit();
-        var h = rt.newChamp();
-
-        var s = std.AutoHashMap(u32, u32).init(alloc);
-        defer s.deinit();
-
-        for (0..m) |i| {
-            const x = rand.intRangeLessThan(u32, 0, n);
-            const y = rand.intRangeLessThan(u32, 0, n);
-            const a = rt.newReal(@floatFromInt(x));
-            const b = rt.newReal(@floatFromInt(y));
-
-            // debugPrint8(a);
-            // debugPrint8(b);
-            // debugPrint8(h);
-            // std.debug.print("{} {}\n", .{ rt.champContains(h, a), s.contains(x) });
-
-            std.debug.assert(rt.champContains(h, a) == s.contains(x));
-            if (rt.champContains(h, a)) {
-                std.debug.assert(
-                    @as(u32, @intFromFloat(rt.champGet(h, a).?.as(.real).data)) == s.get(x).?,
-                );
-                const h2 = rt.champDissoc(h, a);
-                h = h2.?;
-                _ = s.remove(x);
-            } else {
-                const h2 = rt.champAssoc(h, a, b);
-                h = h2;
-                // debugPrint8(h);
-                try s.put(x, y);
-            }
-
-            const u = rand.intRangeLessThan(u32, 0, n);
-            const v = rand.intRangeLessThan(u32, 0, n);
-            const c = rt.newReal(@floatFromInt(u));
-            const d = rt.newReal(@floatFromInt(v));
-            if (rand.boolean()) {
-                const h2 = rt.champDissoc(h, c);
-                h = h2.?;
-                _ = s.remove(u);
-            } else {
-                const h2 = rt.champAssoc(h, c, d);
-                h = h2;
-                try s.put(u, v);
-            }
-
-            if ((i + 1) % o == 0) {
-                rt.gc.traceRoot(&h);
-                rt.gc.shuffle();
-                try rt.gc.collect();
-            }
-        }
     }
 }
 
@@ -262,4 +258,111 @@ fn bench8() !void {
 
         std.debug.print("{}\t{}\t{}\n", .{ n, (ttot - tgc) / m, tgc / m });
     }
+}
+
+fn benchLookup7() !void {
+    var rng = std.rand.DefaultPrng.init(@bitCast(std.time.microTimestamp()));
+    var rand = rng.random();
+    var pool = try BlockPool.init(std.heap.page_allocator);
+    defer pool.deinit();
+
+    const ns = [_]u32{ 32, 128, 512, 2048, 8192, 32768, 131072, 524288 };
+    const m = 100000;
+
+    var timer = try std.time.Timer.start();
+    var acc: usize = 0;
+
+    for (ns) |n| {
+        var rt = try RT7.init(&pool);
+        defer rt.deinit();
+
+        var h = rt.newHamt();
+        var tgc: u64 = 0;
+        for (0..n) |_| {
+            const x = rand.intRangeLessThan(u32, 0, n);
+            const y = rand.intRangeLessThan(u32, 0, n);
+            const a = rt.newReal(@floatFromInt(x));
+            const b = rt.newReal(@floatFromInt(y));
+            const h2 = rt.hamtAssoc(h, a, b);
+            h = h2;
+        }
+
+        timer.reset();
+        for (0..m) |_| {
+            const x = rand.intRangeLessThan(u32, 0, n);
+            const a = rt.newReal(@floatFromInt(x));
+            if (rt.hamtContains(h, a)) acc += 1;
+        }
+        const t1 = timer.read();
+
+        timer.reset();
+        rt.gc.traceRoot(&h);
+        try rt.gc.collect();
+        tgc += timer.read();
+
+        timer.reset();
+        for (0..m) |_| {
+            const x = rand.intRangeLessThan(u32, 0, n);
+            const a = rt.newReal(@floatFromInt(x));
+            if (rt.hamtContains(h, a)) acc += 1;
+        }
+        const t2 = timer.read();
+
+        std.debug.print("{}\t{}\t{}\t{}\n", .{ n, t1 / m, t2 / m, tgc / m });
+    }
+    std.debug.print("{}\n", .{acc});
+}
+
+fn benchLookup8() !void {
+    var rng = std.rand.DefaultPrng.init(@bitCast(std.time.microTimestamp()));
+    var rand = rng.random();
+    var pool = try BlockPool.init(std.heap.page_allocator);
+    defer pool.deinit();
+
+    const ns = [_]u32{ 32, 128, 512, 2048, 8192, 32768, 131072, 524288 };
+    const m = 100000;
+
+    var timer = try std.time.Timer.start();
+    var acc: usize = 0;
+
+    for (ns) |n| {
+        var rt = try RT8.init(&pool);
+        defer rt.deinit();
+
+        var h = rt.newChamp();
+        var tgc: u64 = 0;
+        for (0..n) |_| {
+            const x = rand.intRangeLessThan(u32, 0, n);
+            const y = rand.intRangeLessThan(u32, 0, n);
+            const a = rt.newReal(@floatFromInt(x));
+            const b = rt.newReal(@floatFromInt(y));
+            const h2 = rt.champAssoc(h, a, b);
+            h = h2;
+        }
+
+        timer.reset();
+        for (0..m) |_| {
+            const x = rand.intRangeLessThan(u32, 0, n);
+            const a = rt.newReal(@floatFromInt(x));
+            if (rt.champContains(h, a)) acc += 1;
+        }
+        const t1 = timer.read();
+
+        timer.reset();
+        rt.gc.traceRoot(&h);
+        rt.gc.shuffle();
+        try rt.gc.collect();
+        tgc += timer.read();
+
+        timer.reset();
+        for (0..m) |_| {
+            const x = rand.intRangeLessThan(u32, 0, n);
+            const a = rt.newReal(@floatFromInt(x));
+            if (rt.champContains(h, a)) acc += 1;
+        }
+        const t2 = timer.read();
+
+        std.debug.print("{}\t{}\t{}\t{}\n", .{ n, t1 / m, t2 / m, tgc / m });
+    }
+    std.debug.print("{}\n", .{acc});
 }
